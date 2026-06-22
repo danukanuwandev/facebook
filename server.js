bd97c0ac43424eeea68d2c49890de1208bfea450
Danuka Nuwan <danukanuwanpc@gmail.com>
2026-06-22 19:53:14 +0530

    new

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const https = require('https');
 const { open } = require('sqlite');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
